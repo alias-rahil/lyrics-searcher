0bfe9a679a91776f0981d7cdb78d3b3a6bf9a70a
alias-rahil <rahil.kabani.4@gmail.com>
2021-06-27 03:21:58 +0530

    feat: init

---
+++ bin/index.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env -S node --experimental-specifier-resolution=node --no-warnings
+
+import { program } from "commander";
+import lyricsSearcher from "../dist/index";
+import pkg from "../package";
+
+const { version, description, name } = pkg;
+
+program
+  .name(`npx ${name}`)
+  .version(version)
+  .arguments("<artist> <song>")
+  .description(description, {
+    song: "name of the song",
+    artist: "name of the artist",
+  })
+  .action(async (artist, song) => {
+    try {
+      process.stdout.write(`${await lyricsSearcher(artist, song)}\n`);
+    } catch (e) {
+      process.stderr.write(`${e.message}\n`);
+    }
+  })
+  .allowExcessArguments(false);
+
+program.parseAsync();

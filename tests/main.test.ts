654f15c794eb4211e44f67d7f98666c8304e155e
rahil <rahil.kabani.4@gmail.com>
2021-06-28 07:56:19 +0530

    feat: init

---
+++ tests/main.test.ts
@@ -0,0 +1,16 @@
+import chai from "chai";
+import dirtyChai from "dirty-chai";
+import lyricsSearcher from "../src/lib/index";
+
+chai.use(dirtyChai);
+
+describe('lyricsSearcher("blank space", "taylor swift")', () => {
+  it("should return a non-empty string", (done) => {
+    lyricsSearcher("blank space", "taylor swift")
+      .then((resp) => {
+        chai.expect(!!resp).to.be.true();
+        return done();
+      })
+      .catch((e) => done(e));
+  }).timeout(6000);
+});

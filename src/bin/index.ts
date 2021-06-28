c68cfbcbf155d178fea4153d4dee2c4463b58666
rahil <rahil.kabani.4@gmail.com>
2021-06-28 07:56:19 +0530

    feat: init

---
+++ src/bin/index.ts
@@ -0,0 +1,30 @@
+/* eslint-disable no-console */
+
+import { program } from "commander";
+import lyricsSearcher from "../lib/index";
+import pkg from "../../package.json";
+
+const { name, description, version } = pkg;
+
+program
+  .name(`npx ${name}`)
+  .version(version)
+  .arguments("<artist> <song>")
+  .description(description, {
+    song: "name of the song",
+    artist: "name of the artist",
+  })
+  .action(async (artist, song) => {
+    try {
+      console.log(await lyricsSearcher(artist, song));
+    } catch (e: unknown) {
+      console.error((e as { message: string }).message);
+      process.exit(1);
+    }
+  })
+  .allowExcessArguments(false);
+
+program.parseAsync().catch((e: unknown) => {
+  console.error((e as { message: string }).message);
+  process.exit(1);
+});

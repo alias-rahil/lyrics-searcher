6cbbd9f4c1bdb25e24909589988456fd516df522
rahil <rahil.kabani.4@gmail.com>
2021-06-28 07:56:19 +0530

    feat: init

---
+++ tests/main.test.ts
@@ -0,0 +1,29 @@
+import chai from "chai";
+import dirtyChai from "dirty-chai";
+import shelljs from "shelljs";
+import lyricsSearcher from "../src/lib/index";
+
+chai.use(dirtyChai);
+
+describe('lyricsSearcher("blank space", "taylor swift")', () => {
+  it("should be a non-empty string", (done) => {
+    lyricsSearcher("blank space", "taylor swift")
+      .then((ret) => {
+        chai.expect(!!ret).to.be.true();
+        return done();
+      })
+      .catch((e) => done(e));
+  });
+});
+
+describe('shelljs.exec("npm run --silent cli -- rihanna work", { "silent": true }).stdout.trim()', () => {
+  it("should be a non-empty string", (done) => {
+    const ret = shelljs
+      .exec("npm run --silent cli -- rihanna work", { silent: true })
+      .stdout.trim();
+
+    chai.expect(!!ret).to.be.true();
+
+    return done();
+  });
+});
